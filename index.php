36fac6869d78d5d30be90cce4b34762f3aff8390
robsontech <robson.tech@hotmail.com>
2023-05-10 10:44:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "Olá, turma do az900";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Minha página inicial</title>
+  </head>
+  <body>
+    <h1>Minha página inicial</h1>
+    <p>Bem vindo(a) à minha página inicial! Aqui você encontrará
+      informações sobre mim e meus interesses.</p>
+    <p>Sou um(a) aluno(a) iniciante em programação e cloud computing!
+      Gosto também de assistir filmes, jogar bola, fazer musculação,
+      viajar e jogar vídeo-game.</p>
+    <a href="#" class="button">Entre em contato</a>
+  </body>   
+</html>
